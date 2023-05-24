49dfe3190266dcc29da3eb72f8cff59e00e308df
José Bezerra <jose.bezerra1@academico.ifrn.edu.br>
2023-05-24 17:07:08 -0300

    update socket branch

--- 2023-05-15 - SOCKETS/exercício_sockets.py
+++ 2023-05-15 - SOCKETS/exercício_sockets.py
@@ -19,45 +19,23 @@ def manipular_url(original_url):
     return url_host,url_image,arq_img,arq_txt
 #---------------------------------------------
 
-url_s = manipular_url(input(f'Digite a URL:'))
-print(url_s)
+#url_s = manipular_url(input(f'Digite a URL:'))
+#print(url_s)
 
-url_host    = url_s[0]
-url_image   = url_s[1]
-url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n' 
-host_port   = 80
-buffer_size = 1024
+#url_host    = url_s[0]
+#url_image   = url_s[1]
 
-sock_img = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock_img.connect((url_host, host_port))
-sock_img.sendall(url_request.encode())
+host = 'nasa.gov'
+port = 443
 
-print('\nBaixando a imagem...')
-# Montado a variável que armazenará os dados de retorno
-data_ret = b''
-while True:
-    data = sock_img.recv(buffer_size)
-    if not data: break
-    data_ret += data
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_socket.connect((host , port))
 
-sock_img.close()
+requisicao = f'HEAD / HTTP/1.1\r\nHost: {host}\r\nAccept: text/html\r\nConnection: close\r\n\r\n'
+tcp_socket.sendall(requisicao.encode('utf-8'))
 
-# Obtendo o tamanho da imagem
-img_size = -1
-tmp = data_ret.split('\r\n'.encode())
-for line in tmp:
-   if 'Content-Length:'.encode() in line:
-      img_size = int(line.split()[1])
-      break
-print(f'\nTamanho da Imagem: {img_size} bytes')
+print('-'*100)
+print(str(tcp_socket.recv(1024), 'utf-8'))
+print('-'*100)
 
-# Separando o Cabeçalho dos Dados
-delimiter = '\r\n\r\n'.encode()
-position  = data_ret.find(delimiter)
-headers   = data_ret[:position]
-image     = data_ret[position+4:]
-
-# Salvando a imagem
-file_output = open('image.png', 'wb')
-file_output.write(image)
-file_output.close()
+tcp_socket.close()
