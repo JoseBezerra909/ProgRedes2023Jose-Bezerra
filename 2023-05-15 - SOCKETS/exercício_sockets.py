90fb95f9bbe2c96aab08d6ee9f460e403c5e96fb
José Bezerra <jose.bezerra1@academico.ifrn.edu.br>
2023-05-30 18:13:21 -0300

    update exercícios_socket
    funcional, baixando raw data, próximo passo é baixar a img

--- 2023-05-15 - SOCKETS/exercício_sockets.py
+++ 2023-05-15 - SOCKETS/exercício_sockets.py
@@ -3,39 +3,50 @@ import socket, ssl, sys
 #--------------------------------------------
 #funções
 def manipular_url(original_url):
-    url = original_url.split('.',1)[1]
-    url_host = url.split('/',1)[0]
-    url_image = url.split('/',1)[1]
-    arq_img = url.rsplit('/',1)[1]
-    extensao = arq_img.rsplit('.',1)[1]
-    arq_txt = arq_img.replace(extensao,'txt')
+    url = original_url.split('//',1)
+    s = url[0]
+    url_host = url[1].split('/',1)[0]
+    url_image = '/' + url[1].split('/',1)[1]
+    arq_img = url[1].rsplit('/',1)[1]
+    ext = arq_img.rsplit('.',1)[1]
+    arq_txt = arq_img.replace(ext,'txt')
+    '''
+    print(f'{s}\n'
+        f'{url_host}\n'
+        f'{url_image}\n'
+        f'{arq_img}\n'
+        f'{arq_txt}\n'
+        )'''
+    return s,url_host, url_image, arq_img,arq_txt
 
-    print(f'{url}\n'
-          f'{url_host}\n'
-          f'{url_image}\n'
-          f'{arq_img}\n'
-          f'{arq_txt}\n'
-          )
-    return url_host,url_image,arq_img,arq_txt
 #---------------------------------------------
+#url_s = manipular_url('http://portal.mec.gov.br/images/comunicado/govbr.png')
+#url_s = manipular_url('https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png')
+url_s = manipular_url('https://ead.ifrn.edu.br/portal/wp-content/uploads/2019/03/4Iwakb0M_400x400.png')
+protocolo = url_s[0]
+host = url_s[1]
+image = url_s[2]
+request = bytes(f'GET {image} HTTP/1.1\r\nHost: {host}\r\n\r\n','utf-8')
+buffer = 1024
 
-#url_s = manipular_url(input('Digite URL: '))
-host = 'www.example.com'
-port = 80
-
-try:
+if 'https' in protocolo :
+    port = 443
+    context = ssl.create_default_context()
+    context.check_hostname = 0
+    context.verify_mode = 0
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host,port))
-    request = bytes(f'GET / HTTP/1.1\r\nHost: {host}\r\n\r\n','utf-8')
-    sock.send(request)
-
+    sock_ok = context.wrap_socket(sock, server_hostname=host)
+    sock_ok.connect((host, port))
+    sock_ok.sendall(request)
+    data_ret = b''
     while True:
-        resposta = sock.recv(1024)
-        resposta = resposta.decode('utf-8')
-        print(resposta) 
-        if not resposta: 
-            sock.close()
-            break    
-
-except socket.gaierror as e:
-    print(f'{e}')
+        print('ok')
+        data = sock_ok.recv(buffer)
+        if not data: 
+            print(data_ret)
+            break
+        data_ret += data
+    sock_ok.close()
+else:
+    print('foi por aqui')
+    port = '80'
