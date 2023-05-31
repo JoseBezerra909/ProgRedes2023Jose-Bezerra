c871a29ab2ec89b3036ed4ff76b41ff0fe31766d
José Bezerra <jose.bezerra1@academico.ifrn.edu.br>
2023-05-31 00:23:13 -0300

    Salvar arquivos
    Agora salva os arquivos

---
+++ Socket - File Download/exercício_sockets.py
@@ -0,0 +1,88 @@
+#imports
+import socket, ssl, sys
+#--------------------------------------------
+#funções
+def manipular_url(original_url):
+    url = original_url.split('//',1)
+    s = url[0]
+    url_host = url[1].split('/',1)[0]
+    url_image = '/' + url[1].split('/',1)[1]
+    arq_img = url[1].rsplit('/',1)[1]
+    ext = arq_img.rsplit('.',1)[1]
+    arq_txt = arq_img.replace(ext,'txt')
+    '''
+    print(f'{s}\n'
+        f'{url_host}\n'
+        f'{url_image}\n'
+        f'{arq_img}\n'
+        f'{arq_txt}\n'
+        )'''
+    return s,url_host, url_image, arq_img,arq_txt
+
+def conexao(host,protocolo):
+    socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if 'https' in protocolo:
+        try:
+            print(f'Conexão HTTPS')
+            port = 443
+            context = ssl.create_default_context()
+            context.check_hostname = 0
+            context.verify_mode = 0
+            socketTCP_ok = context.wrap_socket(socketTCP,server_hostname = host)
+            socketTCP_ok.connect((host,port))
+            return socketTCP_ok
+        except:
+            print(f'Erro na conexão HTTP: {sys.exc_info()[0]}')
+            exit()
+    elif 'http' in protocolo:
+        try:
+            print(f'Conexão HTTP')
+            port = 80
+            socketTCP.connect((host,port))
+            return socketTCP
+        except:
+            print(f'Erro na conexão HTTP: {sys.exc_info()[0]}')
+            exit()
+    else:
+        print(f'Protocolo não suportado')
+    return
+
+def salvar(dados,arquivo_png,arquivo_txt):
+    try:
+        image = open(f'Socket - File Download\{arquivo_png}','wb')
+        image.write(dados[1])
+        image.close()
+        head = open(f'Socket - File Download\{arquivo_txt}','wb')
+        head.write(dados[0])
+        head.close()
+        print('criou')
+    except:
+        print(f'Erro: {sys.exc_info()[0]}')
+        exit()
+
+#---------------------------------------------
+#url_s = manipular_url('https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png')
+url_s = manipular_url('https://ead.ifrn.edu.br/portal/wp-content/uploads/2019/03/4Iwakb0M_400x400.png')
+protocolo = url_s[0]
+host = url_s[1]
+image = url_s[2]
+request = bytes(f'GET {image} HTTP/1.1\r\nHost: {host}\r\n\r\n','utf-8')
+buffer = 1024
+
+
+conectado = conexao(host,protocolo)
+conectado.sendall(request)
+data_ret = b''
+try:
+    while True:
+        data = conectado.recv(buffer)
+        data_ret += data
+        if not data:
+            break
+    conectado.close()
+    dados = data_ret.split('\r\n\r\n'.encode())
+    salvar(dados,url_s[3],url_s[4])
+    
+except:
+    print(f'Erro: {sys.exc_info()[0]}')
+    exit()
