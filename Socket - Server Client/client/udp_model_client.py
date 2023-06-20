94ae1e124c8606773eee83b72b552bb29ae9fb04
José Bezerra <jose.bezerra1@academico.ifrn.edu.br>
2023-06-20 16:21:11 -0300

    atulizando cliente_servidor udp

--- Socket - Server Client/client/udp_model_client.py
+++ Socket - Server Client/client/udp_model_client.py
@@ -1,11 +1,15 @@
-import socket
+import socket, time
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True :
-    mensagem = bytes(input('Digite a MSG: '),'utf-8')
-    udp_socket.sendto(mensagem,('localhost',50000))
-    if (mensagem.decode()).lower() == 'exit':
-        udp_socket.close()
+    menu = input(f'BAIXAR ARQUIVOS DIGITE{": 1":>2}\r\nENVIAR MENSAGEM DIGITE{": 2":>2}\r\nPARA SAIR DIGITE {": exit":>11}\r\n>')
+    udp_socket.sendto(menu.encode(),('localhost',60000))
+    if menu.lower() == 'exit':
+        break
+    if menu == '1':
+        time.sleep(1)
+        retorno = eval((udp_socket.recvfrom(4096)[0]).decode())
+        print(retorno)
         break
 
--- Socket - Server Client/server/udp_model_server.py
+++ Socket - Server Client/server/udp_model_server.py
@@ -1,22 +1,39 @@
-import socket,sys,os
+import socket,sys,os, time
 
+
+def lista_arquivos():
+
+    atual_dir = os.path.dirname(__file__) + '\img'
+    arquivos_lista = os.listdir(atual_dir)
+    return arquivos_lista
+
+arquivos = str(lista_arquivos())
+print(arquivos)
+arquivos = eval(arquivos)
+#arquivos = bytes(list(lista_arquivos()), 'utf-8')
+print(arquivos)
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-udp_socket.bind(('localhost', 50000))
+udp_socket.bind(('localhost', 60000))
 
 try:
     print(f'Aguardando MSG:')
     while True:
-        mensagem,ip_client = udp_socket.recvfrom(4096)
-        if (mensagem.decode()).lower() == 'exit':
+        menu,ip_client = udp_socket.recvfrom(4096)
+        print(ip_client)
+        if (menu.decode()).lower() == 'exit':
             print('Fechando Server')
             udp_socket.close()
             break
-        print(f'{ip_client}: {mensagem.decode()}')
+        if menu.decode() == '1':
+            udp_socket.sendto(arquivos,(ip_client[0],ip_client[1]))
+except AttributeError as b:
+    print(b)
+
+except TypeError as e:
+    print(e)
 except:
     udp_socket.close()
     print(f'Erro: {sys.exc_info()[0]}')
 
-
-
-#atual_dir = os.path.dirname(__file__) + '\img' + '\img1.png'
+'''print(f"{ip_client}: {menu.decode()}")'''
