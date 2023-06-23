3d7f663795da7e0e571176391974d76cb8b1b061
José Bezerra <jose.bezerra1@academico.ifrn.edu.br>
2023-06-23 14:06:17 -0300

    V 2.1
    Adição de comentários

--- Socket - Server Client/tcp_model_client.py
+++ Socket - Server Client/tcp_model_client.py
@@ -1,28 +1,47 @@
+#
+#Imports de funções e bibliotecas
+#
 import socket, os, sys
 from constants import *
 
 try: 
+    #
+    #Abrindo socket (Função de abrir conexão está no arquivo constants.py)
+    #
     sock = CONEXAO_CLIENT()
     while True:
+        #
+        #Envio de comando para o server com o nome do arquivo ou parte do nome ou o comando de finalizar operação
+        #
         comunicacao = (input('DIGITE O NOME DO ARQUIVO:\r\nexit p/ Sair\r\n>')).encode()
+        #
+        #Se o comando for exit o programa encerrara a conexão
+        #
         if (comunicacao.decode()).lower() == 'exit':
             sock.send(comunicacao)
             sock.close()
             print(f'Fechando SOCKET')
             break
+        #
+        #Se não o pedido de arquivo será enviado e aguardará o retorno da confirmação e ou de arquivos similares
+        #
         sock.send(comunicacao)
         retorno = (sock.recv(4096)).decode()
         retorno = eval(retorno)
-        
+        #
+        #Se o arquivo não existir o programa mostra arquivos com nome similar
+        #
         if retorno[0] == False:
             print(f'ARQUIVO INEXISTENTE, SUGESTÕES:')
             for x in retorno[1]:
                 print(x)
+        #
+        #Se não o download iniciara e salvara o arquivo
+        #
         else:
             pct = 0
             pct_total = retorno[1]//4096
             print(f'Pacotes a receber: {pct_total}')
-            
             with open(retorno[3],'wb') as arquivo_retorno:
                 while True:
                     sys.stdout.write(f'\rBaixando:{pct} | {pct_total}')
--- Socket - Server Client/tcp_model_server.py
+++ Socket - Server Client/tcp_model_server.py
@@ -1,26 +1,52 @@
-import socket, os, sys,time
+#
+#Imports de funções e bibliotecas
+#
+import socket, os, sys
 from constants import *
 
+#
+#Estabalecendo o local do arquivo e abrindo socket(Função de abrir conexão está no arquivo constants.py)
+#
 diretorios_img = os.path.dirname(__file__)
 sock = CONEXAO_SERVER()
 
 try:
+    #
+    #Aguardando conexão do cliente
+    #
     print(f'Aguardando conexão:')
     conexao,client = sock.accept()
     print(f'Conexão estabelecida com: {client}')
+    
     while True:
+        #
+        #Com o cliente conectado fica aguardando comandos para download de arquivos
+        #
         comunicacao = (conexao.recv(4096)).decode()
+        
+        #
+        #Com o comando 'exit' o client fecha conexão do socket
+        #
         if comunicacao.lower() == 'exit':
             conexao.close()
             sock.close()
             print(f'Fechando SOCKET')
             break
         
+        #
+        #Verifica se o arquivo requisitado existe, se não gera uma lista de arquivos com nomes que encaixem no solicitado
+        #
         verificacao = CHECAR_ARQUIVO(comunicacao,diretorios_img)
-        
         if verificacao[0] == False:
             conexao.send(str(verificacao).encode())
+        
+        #
+        #Se o Arquivo existir o programa vai abrir o arquivo ler e enviar os dados
+        #
         else:
+            #
+            #Enviar o tamanho do arquivo primeiramente para o cliente acompanhar o progresso do download
+            #
             conexao.send(str(verificacao).encode())
             total_data_retorno = 0
             
